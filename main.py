c9ebb160a5ea6d769f5b7780f38dbeeb60c6a853
Agata Marczyk <agamarczyk.agh@gmail.com>
2024-05-27 19:23:47 +0200

    errors

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ from gen.PythonStaticTypingLexer import PythonStaticTypingLexer
 from gen.PythonStaticTypingParser import PythonStaticTypingParser
 from gen.PythonStaticTypingVisitor import PythonStaticTypingVisitor
 from antlr4.error.Errors import RecognitionException, InputMismatchException
+from antlr4.error.ErrorListener import ErrorListener
+
+class ANTLRConsoleErrorListener(ErrorListener):
+    def __init__(self, result_text_widget):
+        super(ANTLRConsoleErrorListener, self).__init__()
+        self.result_text_widget = result_text_widget
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        error_message = f"Syntax Error at line {line}, column {column}: {msg}\n"
+        self.result_text_widget.tag_config("error", foreground="red")
+        self.result_text_widget.insert(tk.END, error_message, "error")
+
 class CodeExecutorApp:
     def __init__(self, root):
         self.root = root
@@ -22,7 +34,7 @@ class CodeExecutorApp:
         self.line_numbers_text.grid(row=0, column=0, sticky="nsew")
 
         self.text_area = scrolledtext.ScrolledText(self.frame, wrap=tk.WORD, width=40, height=40, bg="gray20", fg="dark orange")
-        self.text_area.grid(row=0, column=1, padx=(0,5), sticky="nsew")
+        self.text_area.grid(row=0, column=1, padx=(0, 5), sticky="nsew")
 
         self.frame.rowconfigure(0, weight=1)
         self.frame.columnconfigure(1, weight=1)
@@ -46,9 +58,12 @@ class CodeExecutorApp:
         self.line_numbers_text.insert("1.0", line_numbers)
         self.line_numbers_text.config(state="disabled")
 
+        # Clear the result text when code changes
+        self.result_text.delete("1.0", tk.END)
+
     def get_line_numbers(self):
         line_count = self.text_area.get("1.0", tk.END).count("\n")
-        line_numbers = "\n".join(str(i) for i in range(1, line_count + 2))
+        line_numbers = "\n".join(str(i) for i in range(1, line_count + 1))
         return line_numbers
 
     def execute_code(self):
@@ -66,46 +81,46 @@ class CodeExecutorApp:
             lexer = PythonStaticTypingLexer(input_stream)
             token_stream = CommonTokenStream(lexer)
             parser = PythonStaticTypingParser(token_stream)
+
+            # Dodajemy własny handler do wyświetlania błędów
+            parser.removeErrorListeners()
+            parser.addErrorListener(ANTLRConsoleErrorListener(self.result_text))
+
             tree = parser.program()
 
+            # Wykonanie kodu za pomocą odwiedzającego
             visitor = PythonStaticTypingVisitor()
             visitor.visitProgram(tree)
 
             # Pobranie wyniku z przechwyconego wyjścia
             result = sys.stdout.getvalue()
 
-            # Wyczyszczenie wynikowego pola tekstowego
-            self.result_text.delete("1.0", tk.END)
+            # Pobranie błędów z przechwyconego wyjścia błędów
+            errors = sys.stderr.getvalue()
 
-            # Wyświetlenie wyniku
-            self.result_text.insert(tk.END, result)
+            if errors:
+                # Jeśli wystąpiły błędy, wyświetl je na czerwono
+                self.result_text.tag_config("error", foreground="red")
+                self.result_text.insert(tk.END, errors, "error")
+            else:
+                # W przeciwnym razie wyświetl wynik
+                self.result_text.insert(tk.END, result)
         except InputMismatchException as ime:
-            # Pobranie numeru linii, na której wystąpił błąd składniowy
             error_line = ime.offendingToken.line if ime.offendingToken else -1
-
-            # Pobranie komunikatu błędu składniowego
             error_message = "Mismatched input"
-
-            # Wyświetlenie numeru linii i komunikatu błędu składniowego
             self.result_text.delete("1.0", tk.END)
             self.result_text.insert(tk.END, f"Syntax Error in line {error_line}: {error_message}\n", "error")
         except RecognitionException as re:
-            # Pobranie numeru linii, na której wystąpił błąd składniowy
             error_line = re.offendingToken.line if re.offendingToken else -1
-
-            # Pobranie komunikatu błędu składniowego
             error_message = re.message
-
-            # Wyświetlenie numeru linii i komunikatu błędu składniowego
             self.result_text.delete("1.0", tk.END)
             self.result_text.insert(tk.END, f"Syntax Error in line {error_line}: {error_message}\n", "error")
         except Exception as e:
-            # Pobranie aktualnego numeru linii z okienka tkinter
             current_line_number = int(self.text_area.index(tk.INSERT).split('.')[0])
-
-            # Wyświetlenie numeru linii oraz komunikatu błędu
             self.result_text.delete("1.0", tk.END)
             self.result_text.insert(tk.END, f"Error in line {current_line_number}: {e}\n", "error")
+            # Also capture the full traceback
+            self.result_text.insert(tk.END, "\n" + traceback.format_exc(), "error")
         finally:
             # Przywrócenie standardowego wyjścia i błędów
             sys.stdout = stdout_backup
